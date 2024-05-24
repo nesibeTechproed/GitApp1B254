07114abf7024a3e3b435fe40b02fb2af3b016384
nesibe <tnesibe.techproed@gmail.com>
2024-05-24 23:27:40 +0300

    v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,5 +9,8 @@ public class Runner {
         //versiyon 2
         System.out.println("git commit -m\"\" ile versiyon oluşturulur. ");
 
+        //versiyon 3
+        System.out.println("geçmişe yolculuk");
+
     }
 }
---
+++ src/main/java/Runner02.java
@@ -0,0 +1,11 @@
+public class Runner02 {
+
+    public static void main(String[] args) {
+
+        //versiyon3
+        System.out.println("hello");
+
+
+    }
+
+}
