a49c1c5d9f69eb956a45f2bc7923e18ff0534e60
nesibe <tnesibe.techproed@gmail.com>
2024-05-24 22:23:38 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+
+    public static void main(String[] args) {
+
+        //versiyon 1
+        System.out.println("hello GIT");
+        System.out.println("git add . : staging areaya alır.");
+
+    }
+}
